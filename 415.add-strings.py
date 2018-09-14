24bf425b732a7fcb16b09fa5ff1d729e5a3e0a28
kemu <2540278344@qq.com>
2018-09-14 20:50:28 +0800

    删除

--- 415.add-strings.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def addStrings(self, num1, num2):
-        """
-        :type num1: str
-        :type num2: str
-        :rtype: str
-        """
-        return str(int(num1) + int(num2))
--- 628.maximum-product-of-three-numbers.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def maximumProduct(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        sorted_nums = sorted(nums)
-        return max(sorted_nums[0] * sorted_nums[1] * sorted_nums[-1], sorted_nums[-3] * sorted_nums[-2] * sorted_nums[-1])
