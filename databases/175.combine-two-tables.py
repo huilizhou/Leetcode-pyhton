7009c18047decb7cb0ffeb537ebab11f4ae56e4f
kemu <2540278344@qq.com>
2018-10-16 20:27:12 +0800

    20181016刷题

--- databases/175.combine-two-tables.py
+++
@@ -1,2 +0,0 @@
-# Write your MySQL query statement below
-SELECT FirstName, LastName, City, State FROM Person LEFT JOIN Address ON Person.PersonId = Address.PersonId
--- databases/183.customers-who-never-order.py
+++
@@ -1,2 +0,0 @@
-# Write your MySQL query statement below
-SELECT Name as Customers FROM Customers WHERE ID not in (SELECT C.id FROM Customers as C, Orders as O where C.Id=O.CustomerID)
