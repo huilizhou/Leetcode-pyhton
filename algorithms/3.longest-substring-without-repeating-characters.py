2af26cf1c318e3ee00a136d20760854960974129
kemu <2540278344@qq.com>
2018-11-15 21:17:33 +0800

    20181115

--- algorithms/3.longest-substring-without-repeating-characters.py
+++ algorithms/3.longest-substring-without-repeating-characters.py
@@ -4,14 +4,26 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        longest, start, visited = 0, 0, [False for _ in range(256)]
-        for i, char in enumerate(s):
-            if visited[ord(char)]:
-                while char != s[start]:
-                    visited[ord(s[start])] = False
-                    start += 1
-                start += 1
+        # longest, start, visited = 0, 0, [False for _ in range(256)]
+        # for i, char in enumerate(s):
+        #     if visited[ord(char)]:
+        #         while char != s[start]:
+        #             visited[ord(s[start])] = False
+        #             start += 1
+        #         start += 1
+        #     else:
+        #         visited[ord(char)] = True
+        #     longest = max(longest, i - start + 1)
+        # return longest
+
+        # 使用哈希表
+        hash = {}
+        max_length = 0
+        start = 0
+        for i, string in enumerate(s):
+            if string in hash and start <= hash[string]:
+                start = hash[string] + 1
             else:
-                visited[ord(char)] = True
-            longest = max(longest, i - start + 1)
-        return longest
+                max_length = max(max_length, i - start + 1)
+            hash[string] = i
+        return max_length
