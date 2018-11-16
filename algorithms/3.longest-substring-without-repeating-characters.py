f3c63d608fca85dc7f1dc93e185ce492b43cbcca
kemu <2540278344@qq.com>
2018-11-16 21:54:33 +0800

    20181116刷题

--- algorithms/3.longest-substring-without-repeating-characters.py
+++ algorithms/3.longest-substring-without-repeating-characters.py
@@ -4,18 +4,6 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        # longest, start, visited = 0, 0, [False for _ in range(256)]
-        # for i, char in enumerate(s):
-        #     if visited[ord(char)]:
-        #         while char != s[start]:
-        #             visited[ord(s[start])] = False
-        #             start += 1
-        #         start += 1
-        #     else:
-        #         visited[ord(char)] = True
-        #     longest = max(longest, i - start + 1)
-        # return longest
-
         # 使用哈希表
         hash = {}
         max_length = 0
@@ -27,3 +15,6 @@ class Solution(object):
                 max_length = max(max_length, i - start + 1)
             hash[string] = i
         return max_length
+
+
+print(Solution().lengthOfLongestSubstring("abcasabde"))
---
+++ algorithms/4.median-of-two-sorted-arrays.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        # 我的想法
+        nums1.extend(nums2)
+        nums1.sort()
+        if len(nums1) % 2 == 1:
+            return nums1[len(nums1) // 2]
+        else:
+            return (nums1[len(nums1) // 2 - 1] + nums1[len(nums1) // 2]) / 2.0
+
+
+print(Solution().findMedianSortedArrays([1, 2], [3, 4]))
