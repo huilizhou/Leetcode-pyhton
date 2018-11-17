2cc7d3168cf506f89a75f7a7201549c9608097f8
kemu <2540278344@qq.com>
2018-11-17 11:59:28 +0800

    20181117刷题

---
+++ algorithms/5.longest-palindromic-substring.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        # 运用动态规划的方法，先初始化一字母和二字母的回文，再找到所有3字母的，依次类推
+
+        # 如果字符串长度小于2或者s等于它的倒序，则直接返回s
+        if len(s) < 2 or s == s[::-1]:
+            return s
+        # 定义起始索引和最大回文串长度，odd为奇，even为偶
+        start, max_len = 0, 1
+        for i in range(1, len(s)):
+            # 取i及前面的max_len+2个字符
+            odd = s[i - max_len - 1:i + 1]
+            # 取i及前面max_len+1个字符
+            even = s[i - max_len:i + 1]
+            if i - max_len - 1 >= 0 and odd == odd[::-1]:
+                start = i - max_len - 1
+                max_len += 2
+                continue
+            if i - max_len >= 0 and even == even[::-1]:
+                start = i - max_len
+                max_len += 1
+        return s[start:start + max_len]
+
+
+print(Solution().longestPalindrome('abcbdasdfkk'))
