663262001b4380e6f41f7de99a3481f7cf6a7c7a
kemu <2540278344@qq.com>
2018-12-01 12:55:01 +0800

    20181201刷题

--- algorithms/1-100/048.rotate-image.py
+++ algorithms/1-100/048.rotate-image.py
@@ -4,3 +4,11 @@ class Solution:
         :type matrix: List[List[int]]
         :rtype: void Do not return anything, modify matrix in-place instead.
         """
+        return [list(reversed(x)) for x in zip(*matrix)]
+
+
+print(Solution().rotate(matrix=[
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]))
