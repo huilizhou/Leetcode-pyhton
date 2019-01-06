43de6939593987d227d1fd04bef1c0f9f8a29f74
kemu <2540278344@qq.com>
2019-01-06 10:11:36 +0800

    20190106刷题

---
+++ algorithms/801-900/884.uncommon-words-from-two-sentences.py
@@ -0,0 +1,15 @@
+# 两句话中的不常见单词
+class Solution:
+    def uncommonFromSentences(self, A, B):
+        """
+        :type A: str
+        :type B: str
+        :rtype: List[str]
+        """
+        count = {}
+        for word in A.split():
+            count[word] = count.get(word, 0) + 1
+        for word in B.split():
+            count[word] = count.get(word, 0) + 1
+
+        return [word for word in count if count[word] == 1]
