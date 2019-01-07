158f13331c6534e88c07819f928a80a0cea7da85
kemu <2540278344@qq.com>
2019-01-07 19:18:01 +0800

    20190107刷题

--- algorithms/201-300/223.rectangle-area.py
+++ algorithms/201-300/223.rectangle-area.py
@@ -13,27 +13,25 @@ class Solution:
         :rtype: int
         """
         # 我的写法
-        area1 = (C - A) * (D - B)
-        area2 = (G - E) * (H - F)
-        ma = max(A, E)
-        mb = min(C, G)
-        l1 = mb - ma
-        if l1 <= 0:
-            return area1 + area2
+        # area1 = (C - A) * (D - B)
+        # area2 = (G - E) * (H - F)
+        # ma = max(A, E)
+        # mb = min(C, G)
+        # l1 = mb - ma
+        # if l1 <= 0:
+        #     return area1 + area2
 
-        mc = max(B, F)
-        md = min(D, H)
-        l2 = md - mc
+        # mc = max(B, F)
+        # md = min(D, H)
+        # l2 = md - mc
 
-        if l2 <= 0:
-            return area1 + area2
+        # if l2 <= 0:
+        #     return area1 + area2
 
-        return area1 + area1 - l1 * l2
+        # return area1 + area1 - l1 * l2
 
         # 人家的解法
-        # overlap = max(min(C, G) - max(A, E), 0) * \
-        #     max(min(D, H) - max(B, F), 0)
-        # return (D - B) * (C - A) + (H - F) * (G - E) - overlap
+        return (C - A) * (D - B) + (G - E) * (H - F) - max(0, min(C, G) - max(A, E)) * max(0, min(D, H) - max(B, F))
 
 
 print(Solution().computeArea(-3, 0, 3, 4, 0, -1, 9, 2))
