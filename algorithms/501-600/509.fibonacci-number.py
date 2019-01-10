6b584af4f0ae03d0dac4ec35d515cd0059af689c
kemu <2540278344@qq.com>
2019-01-10 14:36:40 +0800

    20190110刷题

---
+++ algorithms/501-600/509.fibonacci-number.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def fib(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+        # 我的想法
+        # K = [0] + [1] * 30
+        # for i in range(2, len(K)):
+        #     K[i] = K[i - 1] + K[i - 2]
+        # return K[N]
+
+        # 人家的想法，通项公式
+        # return int((5**0.5) * 0.2 * (((1 + 5**0.5) / 2)**N - ((1 - 5**0.5) / 2)**N))
+
+        # 人家的写法
+        if N == 0:
+            return 0
+        elif N == 1:
+            return 1
+        else:
+            return self.fib(N - 1) + self.fib(N - 2)
+
+
+print(Solution().fib(30))
