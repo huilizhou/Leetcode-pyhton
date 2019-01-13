57d83120508acb462d0927aac00cdfa7beeb9bec
kemu <2540278344@qq.com>
2019-01-13 15:47:14 +0800

    20190113刷题

--- algorithms/801-900/821.shortest-distance-to-a-character.py
+++ algorithms/801-900/821.shortest-distance-to-a-character.py
@@ -6,17 +6,27 @@ class Solution:
         :type C: str
         :rtype: List[int]
         """
-        res = [0] * len(S)
-        currentPosition = -len(S)
-        for i in range(len(S)):
+        # res = [0] * len(S)
+        # currentPosition = -len(S)
+        # for i in range(len(S)):
+        #     if S[i] == C:
+        #         currentPosition = i
+        #     res[i] = i - currentPosition
+        # for i in range(len(S) - 1, -1, -1):
+        #     if S[i] == C:
+        #         currentPosition = i
+        #     res[i] = min(res[i], abs(currentPosition - i))
+        # return res
+
+        # 人家的解法
+        import itertools
+        result = [len(S)] * len(S)
+        prev = -len(S)
+        for i in itertools.chain(range(len(S)), reversed(range(len(S)))):
             if S[i] == C:
-                currentPosition = i
-            res[i] = i - currentPosition
-        for i in range(len(S) - 1, -1, -1):
-            if S[i] == C:
-                currentPosition = i
-            res[i] = min(res[i], abs(currentPosition - i))
-        return res
+                prev = i
+            result[i] = min(result[i], abs(i - prev))
+        return result
 
 
 print(Solution().shortestToChar(S="loveleetcode", C='e'))
