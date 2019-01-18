72bf4dde56b07115b184546bd0920052e75f1a9e
kemu <2540278344@qq.com>
2019-01-18 14:49:01 +0800

    20190118modify

--- algorithms/101-200/0000_test.py
+++ algorithms/101-200/0000_test.py
@@ -54,33 +54,33 @@
 # print(Solution().shell_sort([2, 1, 3, 7, 5, 6, 4]))
 
 
-# 堆排序
-class Solution:
-    def heap_sort(self, list):
-        '''最大堆调整'''
-        def shift_down(start, end):
-            root = start
-            while True:
-                child = root * 2 + 1
-                if child > end:
-                    break
-                if child + 1 <= end and list[child] < list[child + 1]:
-                    child += 1
-                if list[root] < list[child]:
-                    list[child], list[root] = list[root], list[child]
-                    root = child
-                else:
-                    break
+# # 堆排序
+# class Solution:
+#     def heap_sort(self, list):
+#         '''最大堆调整'''
+#         def shift_down(start, end):
+#             root = start
+#             while True:
+#                 child = 2 * root + 1
+#                 if child > end:
+#                     break
+#                 if child + 1 <= end and list[child] < list[child + 1]:
+#                     child += 1
+#                 if list[root] < list[child]:
+#                     list[child], list[root] = list[root], list[child]
+#                     root = child
+#                 else:
+#                     break
 
-        # 构建最大堆
-        for start in range((len(list) - 2) // 2, -1, -1):
-            shift_down(start, len(list) - 1)
+#         # 构建最大堆
+#         for start in range((len(list) - 2) // 2, -1, -1):
+#             shift_down(start, len(list) - 1)
 
-        # 堆排序
-        for end in range(len(list) - 1, 0, -1):
-            list[0], list[end] = list[end], list[0]
-            shift_down(0, end - 1)
-        return list
+#         # 堆排序
+#         for end in range(len(list) - 1, 0, -1):
+#             list[0], list[end] = list[end], list[0]
+#             shift_down(0, end - 1)
+#         return list
 
 
-print(Solution().heap_sort([9, 2, 1, 7, 6, 8, 5, 3, 4]))
+# print(Solution().heap_sort([9, 3, 2, 1, 4, 5, 6, 8, 7]))
