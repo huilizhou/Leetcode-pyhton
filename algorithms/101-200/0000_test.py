b3cd00e33df6dcff7720342a4821b038cca42ecb
kemu <2540278344@qq.com>
2019-01-18 14:42:09 +0800

    20190118sort

--- algorithms/101-200/0000_test.py
+++ algorithms/101-200/0000_test.py
@@ -34,20 +34,53 @@
 # a = {1, 2, 3, 4, 5, 6}
 # print(a)
 
+# 希尔排序
+# class Solution:
+#     def shell_sort(self, list):
+#         n = len(list)
+#         gap = n // 2
+#         while gap > 0:
+#             for i in range(gap, n):
+#                 temp = list[i]
+#                 j = i
+#                 while j >= gap and list[j - gap] > temp:
+#                     list[j] = list[j - gap]
+#                     j -= gap
+#                 list[j] = temp
+#             gap = gap // 2
+#         return list
+
+
+# print(Solution().shell_sort([2, 1, 3, 7, 5, 6, 4]))
+
+
+# 堆排序
 class Solution:
-    def shell_sort(self, list):
-        n = len(list)
-        gap = n // 2
-        while gap > 0:
-            for i in range(gap, n):
-                temp = list[i]
-                j = i
-                while j >= gap and list[j - gap] > temp:
-                    list[j] = list[j - gap]
-                    j -= gap
-                list[j] = temp
-            gap = gap // 2
+    def heap_sort(self, list):
+        '''最大堆调整'''
+        def shift_down(start, end):
+            root = start
+            while True:
+                child = root * 2 + 1
+                if child > end:
+                    break
+                if child + 1 <= end and list[child] < list[child + 1]:
+                    child += 1
+                if list[root] < list[child]:
+                    list[child], list[root] = list[root], list[child]
+                    root = child
+                else:
+                    break
+
+        # 构建最大堆
+        for start in range((len(list) - 2) // 2, -1, -1):
+            shift_down(start, len(list) - 1)
+
+        # 堆排序
+        for end in range(len(list) - 1, 0, -1):
+            list[0], list[end] = list[end], list[0]
+            shift_down(0, end - 1)
         return list
 
 
-print(Solution().shell_sort([2, 1, 3, 7, 5, 6, 4]))
+print(Solution().heap_sort([9, 2, 1, 7, 6, 8, 5, 3, 4]))
--- algorithms/sort/heap_sort.py
+++ algorithms/sort/heap_sort.py
@@ -6,6 +6,13 @@
 堆是一个近似完全二叉树的结构，并同时满足堆积的性质：
 即子结点的键值或索引总是小于（或者大于）它的父节点。
 
+堆节点的访问
+通常堆是通过一维数组来实现的。在数组的起始位置为0的情形中：
+- 父节点i的左子节点的位置(2i+1)
+- 父节点i的右子节点的位置(2i+2)
+- 子节点i的父节点位置为(i-1)//2
+
+
 1.创建最大堆:将堆所有数据重新排序，使其成为最大堆
 2.最大堆调整:作用是保持最大堆的性质，是创建最大堆的核心子程序
 3.堆排序:移除位在第一个数据的根节点，并做最大堆调整的递归运算
@@ -14,7 +21,7 @@
 
 
 class Solution:
-    def heap_sort(self, lst):
+    def heap_sort(self, list):
         def sift_down(start, end):
             """最大堆调整"""
             root = start
@@ -22,23 +29,23 @@ class Solution:
                 child = 2 * root + 1
                 if child > end:
                     break
-                if child + 1 <= end and lst[child] < lst[child + 1]:
+                if child + 1 <= end and list[child] < list[child + 1]:
                     child += 1
-                if lst[root] < lst[child]:
-                    lst[root], lst[child] = lst[child], lst[root]
+                if list[root] < list[child]:
+                    list[root], list[child] = list[child], list[root]
                     root = child
                 else:
                     break
 
     # 创建最大堆
-        for start in range((len(lst) - 2) // 2, -1, -1):
-            sift_down(start, len(lst) - 1)
+        for start in range((len(list) - 2) // 2, -1, -1):
+            sift_down(start, len(list) - 1)
 
     # 堆排序
-        for end in range(len(lst) - 1, 0, -1):
-            lst[0], lst[end] = lst[end], lst[0]
+        for end in range(len(list) - 1, 0, -1):
+            list[0], list[end] = list[end], list[0]
             sift_down(0, end - 1)
-        return lst
+        return list
 
 
-print(Solution().heap_sort([3, 2, 4, 1, 5, 6, 10, 8]))
+print(Solution().heap_sort([9, 2, 1, 7, 6, 8, 5, 3, 4]))
