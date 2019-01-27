8515d775430d85c375af7e3b3ad2c0b0830f0951
kemu <2540278344@qq.com>
2019-01-27 17:15:31 +0800

    20190127刷题

---
+++ algorithms/201-300/239.sliding-window-maximum.py
@@ -0,0 +1,37 @@
+class Solution:
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        # 人家的想法
+        # from collections import deque
+        # dq = deque()
+        # max_numbers = []
+
+        # for i in range(len(nums)):
+        #     while dq and nums[i] >= nums[dq[-1]]:
+        #         dq.pop()
+        #     dq.append(i)
+        #     if i >= k and dq and dq[0] == i - k:
+        #         dq.popleft()
+        #     if i >= k - 1:
+        #         max_numbers.append(nums[dq[0]])
+
+        # return max_numbers
+
+        if not nums or k <= 0 or k > len(nums):
+            return []
+        res = [max(nums[:k])]
+        max_val = res[0]
+        for i in range(k, len(nums)):
+            if nums[i] > max_val:
+                max_val = nums[i]
+            elif nums[i - k] == max_val:
+                max_val = max(nums[i - k + 1:i + 1])
+            res.append(max_val)
+        return res
+
+
+print(Solution().maxSlidingWindow(nums=[1, 3, -1, -3, 5, 3, 6, 7], k=3))
