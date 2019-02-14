352fddd0a4a01a9dbd05f731f6da3b053dfed1b9
kemu <2540278344@qq.com>
2019-02-14 11:51:46 +0800

    20190214刷题

---
+++ algorithms/801-900/814.binary-tree-pruning.py
@@ -0,0 +1,56 @@
+# 二叉树剪枝
+# Definition for a binary tree node.
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    def __repr__(self):
+        if self:
+            serial = []
+            queue = [self]
+
+            while queue:
+                cur = queue[0]
+
+                if cur:
+                    serial.append(cur.val)
+                    queue.append(cur.left)
+                    queue.append(cur.right)
+                else:
+                    serial.append("#")
+
+                queue = queue[1:]
+
+            while serial[-1] == "#":
+                serial.pop()
+
+            return repr(serial)
+
+        else:
+            return None
+
+
+class Solution:
+    def pruneTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
+        if not root.left and not root.right and root.val == 0:
+            return None
+        return root
+
+
+tree = TreeNode(1)
+tree.right = TreeNode(0)
+tree.right.left = TreeNode(0)
+tree.right.right = TreeNode(1)
+print(Solution().pruneTree(tree))
