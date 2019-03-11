6e73b68d93bdbaff8eed3be5d3d9a9ea7f17691d
kemu <2540278344@qq.com>
2019-03-11 13:52:22 +0800

    20190311modify

--- algorithms/1-100/021.merge-two-sorted-lists.py
+++ algorithms/1-100/021.merge-two-sorted-lists.py
@@ -35,19 +35,19 @@ class Solution:
         # return result
 
         # 人家解法
-        # curr = dummy = ListNode(0)
+        curr = dummy = ListNode(0)
 
-        # while l1 and l2:
-        #     if l1.val < l2.val:
-        #         curr.next = l1
-        #         l1 = l1.next
-        #     else:
-        #         curr.next = l2
-        #         l2 = l2.next
-        #     curr = curr.next
-        # curr.next = l1 or l2
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        curr.next = l1 or l2
 
-        # return dummy.next
+        return dummy.next
 
         # 人家的解法，采用递归的思想
         # if l1 == None:
--- algorithms/1-100/046.permutations.py
+++ algorithms/1-100/046.permutations.py
@@ -4,11 +4,11 @@ class Solution:
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        # 最简单的做法
-        if len(nums) <= 1:
-            return [nums]
-        import itertools
-        return list(itertools.permutations(nums, len(nums)))
+        # 最简单的做法，调用itertools
+        # if len(nums) <= 1:
+        #     return [nums]
+        # import itertools
+        # return list(itertools.permutations(nums, len(nums)))
 
         # 人家的解法
     #     result = []
@@ -28,15 +28,16 @@ class Solution:
     #             cur.pop()
     #             used[i] = False
 
-        # 我的另一种想法
-        # if len(nums) <= 1:
-        #     return [nums]
-        # ans = []
-        # for i, num in enumerate(nums):
-        #     n = nums[:i] + nums[i + 1:]
-        #     for temp_list in self.permute(n):
-        #         ans.append([num] + temp_list)
-        # return ans
+        # 我看了人家解法后的想法
+        res = []
+        if len(nums) <= 1:
+            return [nums]
+        for i in range(len(nums)):
+            num = nums[i]
+            newnums = nums[:i] + nums[i + 1:]
+            for item in self.permute(newnums):
+                res.append([num] + item)
+        return res
 
 
 print(Solution().permute([1, 2, 3]))
