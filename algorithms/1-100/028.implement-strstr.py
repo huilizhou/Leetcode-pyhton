7688ee149c64bb243ac48bab8d2ba3f917b81377
kemu <2540278344@qq.com>
2019-03-21 09:26:36 +0800

    20190321modify

--- algorithms/1-100/028.implement-strstr.py
+++ algorithms/1-100/028.implement-strstr.py
@@ -6,18 +6,23 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
+        '''
+        给定一个haystack字符串和一个needle字符串，
+        在haystack字符串中找出needle字符串出现的第一个位置
+        (从0开始)。如果不存在，则返回-1
+        '''
         # 我的想法
-        if not needle:
-            return 0
-        elif needle not in haystack:
-            return -1
-        elif needle in haystack:
-            return haystack.find(needle)
+        # if not needle:
+        #     return 0
+        # elif needle not in haystack:
+        #     return -1
+        # elif needle in haystack:
+        #     return haystack.find(needle)
 
-        # for i in range(len(haystack) - len(needle) + 1):
-        #     if haystack[i:i + len(needle)] == needle:
-        #         return i
-        # return -1
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i + len(needle)] == needle:
+                return i
+        return -1
 
 
 print(Solution().strStr(haystack="hello", needle="ll"))
--- algorithms/1-100/039.combination-sum.py
+++ algorithms/1-100/039.combination-sum.py
@@ -1,3 +1,4 @@
+# 组合总和
 class Solution(object):
     def combinationSum(self, candidates, target):
         """
@@ -5,49 +6,36 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
-    #     candidates.sort()
-    #     # 储存结果
-    #     Solution.anslist = []
-    #     self.DFS(candidates, target, 0, [])
-    #     return Solution.anslist
+        res = self.dfs(candidates, target)
+        return res
 
-    # def DFS(self, candidates, target, start, valuelist):
-    #     if target == 0:
-    #         return Solution.anslist.append(valuelist)
-    #     for i in range(start, len(candidates)):
-    #         # 注意在我们的递归函数中，target是不断在变化的
-    #         # 因为每次我们调用递归都要用target减去candidates[i],所以这时候如果不保证target比较大，这一定不符合我们的要求
-    #         if candidates[i] > target:
-    #             return
-    #         # 递归时我们的减少的条件是target，每次它都会减少
-    #         # 如何做到题目说的一个数字可以多次取呢？
-    #         # 我们设置了一个start，它会保存上一次取的i,这一次可以继续取，如果符合条件的话
-    #         self.DFS(candidates, target -
-    #                  candidates[i], i, valuelist + [candidates[i]])
+    def dfs(self, candidates, target):
+        res = []
+        for i in range(len(candidates)):
+            if candidates[i] > target:
+                continue
+            if candidates[i] == target:
+                res.append([candidates[i]])
+            for item in self.dfs(candidates[i:], target - candidates[i]):
+                res.append([candidates[i]] + item)
+        return res
 
-        # 人家的解法
-        candidates.sort()
-
-        def findSum(cans, tar):
-            result_list = []
-            for i, can in enumerate(cans):
-                # print(cans, tar)
-                can = cans[i]
-                if tar == can:
-                    result_list.append([can])
-                elif tar > can:
-                    found = findSum(cans[i:], tar - can)
-                    if found:
-                        for sum_list in found:
-                            sum_list.append(can)
-                            result_list.append(sum_list)
-                else:
-                    return result_list
-            return result_list
-        return findSum(candidates, target)
+        # 我的想法，递归
+        # '''
+        # 我的想法递归
+        # '''
+        # if target < 0:
+        #     return []
+        # res = []
+        # for i, a in enumerate(candidates):
+        #     if a == target:
+        #         res.append([a])
+        #     for j in self.combinationSum(candidates[i:], target - a):
+        #         res.append([a] + j)
+        # return res
 
 
 if __name__ == "__main__":
-    candidates, target = [2, 3, 6, 7], 7
+    candidates, target = [1, 2, 5, 10, 20, 50, 100], 100
     result = Solution().combinationSum(candidates, target)
     print(result)
