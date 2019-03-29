ce50ad9c0ac1538ec5142d5c0b2f4a202598ef3c
kemu <2540278344@qq.com>
2019-03-29 12:16:17 +0800

    20190329modify

--- algorithms/1-100/035.search-insert-position.py
+++ algorithms/1-100/035.search-insert-position.py
@@ -6,12 +6,33 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
+        # 我的想法
+        # if target in nums:
+        #     return nums.index(target)
+        # else:
+        #     nums.append(target)
+        #     return sorted(nums).index(target)
 
-        if target in nums:
-            return nums.index(target)
-        else:
-            nums.append(target)
-            return sorted(nums).index(target)
+        # 不用内置函数的做法
+        # l, r = 0, len(nums) - 1
+        # while l <= r:
+        #     mid = (l + r) // 2
+        #     if target == nums[mid]:
+        #         return mid
+        #     if target > nums[mid]:
+        #         l += 1
+        #     else:
+        #         r -= 1
+        # return l
+
+        for index, value in enumerate(nums):
+            if target == value:
+                return index
+            else:
+                if target < value:
+                    return index
+                elif target > nums[-1]:
+                    return len(nums)
 
 
-print(Solution().searchInsert([1, 3, 5, 6], 7))
+print(Solution().searchInsert([1, 3, 5, 6], 4))
