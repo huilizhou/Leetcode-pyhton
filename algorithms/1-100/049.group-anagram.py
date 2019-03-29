5920b67ca497c27c07437fcfcd7d9a7ccf0f3ec4
kemu <2540278344@qq.com>
2019-03-29 13:36:48 +0800

    201903299刷题

--- algorithms/1-100/049.group-anagram.py
+++ algorithms/1-100/049.group-anagram.py
@@ -5,27 +5,15 @@ class Solution:
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        # import collections
-        # anagrams_map, result = collections.defaultdict(list), []
-        # for s in strs:
-        #     sorted_str = ("").join(sorted(s))
-        #     anagrams_map[sorted_str].append(s)
-        # for anagram in anagrams_map.values():
-        #     anagram.sort()
-        #     result.append(anagram)
-        # return result
-
-        # 人家的解法，厉害
-        str_map = {}
-        res = []
-        for i in strs:
-            string = ''.join(sorted(i))
-            if string not in str_map:
-                str_map[string] = len(res)
-                res.append([i])
+        # # 使用哈希表，键值为排序后的字母异位词
+        dic = {}
+        for x in strs:
+            sorted_x = "".join(sorted(x))
+            if sorted_x in dic:
+                dic[sorted_x].append(x)
             else:
-                res[str_map[string]].append(i)
-        return res
+                dic[sorted_x] = [x]
+        return list(dic.values())
 
 
 print(Solution().groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
