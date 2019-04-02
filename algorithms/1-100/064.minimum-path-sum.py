a16ce0b6963cbfa81d49729ef3e63e053fe6e07b
kemu <2540278344@qq.com>
2019-04-02 17:45:27 +0800

    20190402modify

--- algorithms/1-100/064.minimum-path-sum.py
+++ algorithms/1-100/064.minimum-path-sum.py
@@ -5,9 +5,9 @@ class Solution:
         :type grid: List[List[int]]
         :rtype: int
         """
-        # 我的想法
-        m = len(grid)
-        n = len(grid[0])
+        # 我的想法，动态规划的解法
+        m = len(grid)  # row
+        n = len(grid[0])  # column
         for i in range(m):
             for j in range(n):
                 if i == 0 and j == 0:
@@ -17,23 +17,8 @@ class Solution:
                 elif j == 0 and i > 0:
                     grid[i][j] += grid[i - 1][j]
                 elif i > 0 and j > 0:
-                    grid[i][j] = min(grid[i][j] + grid[i - 1]
-                                     [j], grid[i][j] + grid[i][j - 1])
-        return grid[m - 1][n - 1]
-
-        # 人家的写法
-        # m = len(grid)
-        # n = len(grid[0])
-        # res = [[0] * n for _ in range(m)]
-        # res[0][0] = grid[0][0]
-        # for i in range(1, m):
-        #     res[i][0] = grid[i][0] + res[i - 1][0]
-        # for j in range(1, n):
-        #     res[0][j] = grid[0][j] + res[0][j - 1]
-        # for i in range(1, m):
-        #     for j in range(1, n):
-        #         res[i][j] = min(res[i][j - 1], res[i - 1][j]) + grid[i][j]
-        # return res[m - 1][n - 1]
+                    grid[i][j] += min(grid[i - 1][j], grid[i][j - 1])
+        return grid[- 1][- 1]
 
 
 print(Solution().minPathSum([
