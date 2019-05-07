f7eac7ec2f4a4b6643e9fcff791524c0c48d62f2
kemu <2540278344@qq.com>
2019-05-07 15:00:36 +0800

    20190507modify

--- algorithms/401-500/498.diagonal-tracerse.py
+++ algorithms/401-500/498.diagonal-tracerse.py
@@ -7,29 +7,81 @@ class Solution:
         # 分析思路
         # 索引和为偶数：元素在第一行，往右走，元素在最后一列，往下走，其他情况往右上走
         # 索引和为奇数，元素在第一列，往下走，元素在最后一行，往右走，其他情况往左下走
-        res = []
-        if not matrix:
+
+        # res = []
+        # if not matrix:
+        #     return []
+
+        # m = len(matrix)
+        # n = len(matrix[0])
+
+        # for level in range(m + n - 1):
+        #     if level % 2 == 0:
+        #         i = min(m - 1, level)
+        #         j = level - i
+        #         while i >= 0 and j < n:
+        #             res.append(matrix[i][j])
+        #             i -= 1
+        #             j += 1
+        #     else:
+        #         j = min(n - 1, level)
+        #         i = level - j
+        #         while j >= 0 and i < m:
+        #             res.append(matrix[i][j])
+        #             j -= 1
+        #             i += 1
+        # return res
+
+        # if len(matrix) == 0:
+        #     return []
+        # M, N = len(matrix), len(matrix[0])
+        # ans = [0] * (M * N)
+        # row, col = 0, 0
+
+        # for i in range(M * N):
+        #     ans[i] = matrix[row][col]
+        #     if (row + col) % 2 == 0:
+        #         if col == N - 1:
+        #             row += 1
+        #         elif row == 0:
+        #             col += 1
+        #         else:
+        #             col += 1
+        #             row -= 1
+        #     else:
+        #         if row == M - 1:
+        #             col += 1
+        #         elif col == 0:
+        #             row += 1
+        #         else:
+        #             col -= 1
+        #             row += 1
+        # return ans
+
+        if len(matrix) == 0:
             return []
-
-        m = len(matrix)
-        n = len(matrix[0])
-
-        for level in range(m + n - 1):
-            if level % 2 == 0:
-                i = min(m - 1, level)
-                j = level - i
-                while i >= 0 and j < n:
-                    res.append(matrix[i][j])
-                    i -= 1
-                    j += 1
+        M, N = len(matrix), len(matrix[0])
+        ans = [0] * (M * N)
+        row, col = 0, 0
+        for i in range(M * N):
+            ans[i] = matrix[row][col]
+            if (row + col) % 2 == 0:
+                if col == N - 1:
+                    row += 1
+                elif row == 0:
+                    col += 1
+                else:
+                    row -= 1
+                    col += 1
             else:
-                j = min(n - 1, level)
-                i = level - j
-                while j >= 0 and i < m:
-                    res.append(matrix[i][j])
-                    j -= 1
-                    i += 1
-        return res
+                if row == M - 1:
+                    col += 1
+                elif col == 0:
+                    row += 1
+                else:
+                    row += 1
+                    col -= 1
+        return ans
 
 
 print(Solution().findDiagonalOrder([
