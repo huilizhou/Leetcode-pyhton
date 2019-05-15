9484796c28b0bcadc6457cdcc704c6e14c58646e
kemu <2540278344@qq.com>
2019-05-15 10:41:50 +0800

    20190515bukaixin

--- algorithms/301-400/0000_test.py
+++ algorithms/301-400/0000_test.py
@@ -6,7 +6,3 @@
 # print(1 << (ord("c") - 97))
 # print(0 | 1)
 # print(~60)
-amount = 11
-coins = [1, 2, 5]
-dp = [[0 for _ in range(amount + 1)] for _ in range(len(coins))]
-print(dp)
--- algorithms/401-500/0000_test.py
+++ algorithms/401-500/0000_test.py
@@ -17,3 +17,16 @@
 # a[:] = [101, 102, 103]
 # print(a)
 # print("ida=", id(a), "\n", id(b))
+
+
+# nums = "sssaabcdd"
+# import collections
+# nums1 = collections.Counter(nums)
+# for k, v in nums1.most_common():
+#     print(k, v)
+# print(nums1)
+
+
+# dic = {'a': 1, 'b': 2, 'c': 1}
+# for k, v in dic.items():
+#     print(k, v)
