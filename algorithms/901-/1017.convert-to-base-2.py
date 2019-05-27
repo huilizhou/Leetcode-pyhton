2f756d121dcc16f68913933e193a4808dbc9bce3
kemu <2540278344@qq.com>
2019-05-27 09:43:12 +0800

    20190527modify

--- algorithms/901-/1017.convert-to-base-2.py
+++ algorithms/901-/1017.convert-to-base-2.py
@@ -4,13 +4,30 @@ class Solution(object):
         :type N: int
         :rtype: str
         """
-        res = ""
-        while N:
-            tmp = N % 2
-            res = str(tmp) + res
-            N = -1 * (N // 2)
+        # res = ""
+        # while N:
+        #     tmp = N % 2
+        #     res = str(tmp) + res
+        #     N = -1 * (N // 2)
 
-        return res if res else "0"
+        # return res if res else "0"
+
+        # 人家的解法
+        if N == 0:
+            return '0'
+        res = ''
+        while 1:
+            b = N % (-2)
+            if N == 1:
+                res += '1'
+                return res[::-1]
+
+            if b == 0:
+                res += ('0')
+                N = N // (-2)
+            else:
+                res += ('1')
+                N = N // (-2) + 1
 
 
-print(Solution().baseNeg2(2))
+print(Solution().baseNeg2(8))
--- algorithms/901-/1029.two-city-scheduling.py
+++ algorithms/901-/1029.two-city-scheduling.py
@@ -5,14 +5,28 @@ class Solution(object):
         :type costs: List[List[int]]
         :rtype: int
         """
-        res = 0
-        diff = [0] * len(costs)
-        for i in range(len(costs)):
-            res += costs[i][0]
-            diff[i] = costs[i][1] - costs[i][0]
-        diff.sort()
-        for i in range(len(costs) // 2):
-            res += diff[i]
+
+        '''
+        第一步：先所有人都去A市，此时记录B市与A市的差价
+        第二步：将B与A的差价按从小到达排序。
+        第三步：所有的值加上一半的差价前面的值。
+
+        '''
+        # res = 0
+        # diff = [0] * len(costs)
+        # for i in range(len(costs)):
+        #     res += costs[i][0]
+        #     diff[i] = costs[i][1] - costs[i][0]
+        # diff.sort()
+        # for i in range(len(costs) // 2):
+        #     res += diff[i]
+        # return res
+
+        # 人家的解法
+        costs = sorted(costs, key=lambda x: x[0] - x[1])
+        res, n = 0, len(costs) // 2
+        for i in range(n):
+            res += costs[i][0] + costs[i + n][1]
         return res
 
 
