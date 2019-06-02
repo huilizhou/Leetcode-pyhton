dc8301a784801b40e55dd9438bfc04ac860acb89
kemu <2540278344@qq.com>
2019-06-02 09:46:20 +0800

    20190602刷题

---
+++ algorithms/901-/912.sort-an-array.py
@@ -0,0 +1,9 @@
+# 排序数组
+class Solution(object):
+    def sortArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums.sort()
+        return nums
