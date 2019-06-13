32b23282e16add8aa0c90dddf63e0d20e4159de5
kemu <2540278344@qq.com>
2019-06-13 20:05:26 +0800

    20190613刷题

--- algorithms/301-400/315.count-of-smaller-numbers-after-self.py
+++ algorithms/301-400/315.count-of-smaller-numbers-after-self.py
@@ -1,22 +1,22 @@
 # 计算右侧小于当前元素的个数
 class Solution(object):
-    # def countSmaller(self, nums):
-    #     """
-    #     :type nums: List[int]
-    #     :rtype: List[int]
-    #     """
-    #     import bisect
+    def countSmaller(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        import bisect
 
-    #     count_list = []
-    #     result_list = []
-    #     nums.reverse()
+        count_list = []
+        result_list = []
+        nums.reverse()
 
-    #     for i in nums:
-    #         index = bisect.bisect_left(count_list, i)
-    #         count_list.insert(index, i)
-    #         result_list.append(index)
-    #     result_list.reverse()
-    #     return result_list
+        for num in nums:
+            index = bisect.bisect_left(count_list, num)
+            count_list.insert(index, num)
+            result_list.append(index)
+        result_list.reverse()
+        return result_list
 
     # # 我的解法，有一个测试用例超时，时间复杂度为O(n^2)
     # res = []
@@ -31,27 +31,27 @@ class Solution(object):
     # res.append(0)
     # return res
 
-    def bisearch(self, tmp, a):
-        left = 0
-        right = len(tmp)
-        while left < right:
-            mid = (left + right) // 2
-            if tmp[mid] < a:
-                left = mid + 1
-            else:
-                right = mid
-        return left
+    # def bisearch(self, tmp, a):
+    #     left = 0
+    #     right = len(tmp)
+    #     while left < right:
+    #         mid = (left + right) // 2
+    #         if tmp[mid] < a:
+    #             left = mid + 1
+    #         else:
+    #             right = mid
+    #     return left
 
-    def countSmaller(self, nums):
-        ans = []
-        tmp = []
+    # def countSmaller(self, nums):
+    #     ans = []
+    #     tmp = []
 
-        for i in range(len(nums) - 1, -1, -1):
-            pos = self.bisearch(tmp, nums[i])
-            ans.append(pos)
-            tmp.insert(pos, nums[i])
-        ans.reverse()
-        return ans
+    #     for i in range(len(nums) - 1, -1, -1):
+    #         pos = self.bisearch(tmp, nums[i])
+    #         ans.append(pos)
+    #         tmp.insert(pos, nums[i])
+    #     ans.reverse()
+    #     return ans
 
 
-print(Solution().countSmaller([5, 2, 6, 1]))
+print(Solution().countSmaller([5, 2, 6, 1, 3]))
