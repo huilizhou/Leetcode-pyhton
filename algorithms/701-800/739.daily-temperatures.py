c9a69099aaa5b623ce807b78327b0c7d6758a84f
kemu <2540278344@qq.com>
2019-06-15 11:27:02 +0800

    20190615刷题

---
+++ algorithms/701-800/739.daily-temperatures.py
@@ -0,0 +1,21 @@
+#  每日温度
+class Solution(object):
+    def dailyTemperatures(self, T):
+        """
+        :type T: List[int]
+        :rtype: List[int]
+        """
+        # 入栈条件：当前元素比栈顶元素小，出栈条件：遇到比自己大的温度，出栈时索引距离即天数差
+
+        stack = []
+        ret = [0] * len(T)
+        for i in range(len(T)):
+            while stack and T[stack[-1]] < T[i]:
+                ret[stack[-1]] = i - stack[-1]
+                stack.pop()
+            stack.append(i)
+        return ret
+
+
+print(Solution().dailyTemperatures(
+    [73, 74, 75, 71, 69, 72, 76, 73]))
