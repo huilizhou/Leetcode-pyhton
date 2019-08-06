159cbaae0a89399d5b8acf67530de000d53900bb
kemu <2540278344@qq.com>
2019-08-06 23:17:12 +0800

    20190806刷题

--- algorithms/401-500/485.max-consecutive-ones.py
+++ algorithms/401-500/485.max-consecutive-ones.py
@@ -1,3 +1,4 @@
+# 最大连续1的个数
 class Solution:
     def findMaxConsecutiveOnes(self, nums):
         """
@@ -6,29 +7,28 @@ class Solution:
         """
 
         # # 我的想法
-        # res = 0
-        # count = 0
-        # for i in nums:
-        #     if i == 1:
-        #         count += 1
-        #     else:
-        #         count = 0
-        #     if count > res:
-        #         res = count
-        # return res
+        res = 0
+        count = 0
+        for i in nums:
+            if i == 1:
+                count += 1
+            else:
+                res = max(res, count)
+                count = 0
+        return max(res, count)
 
         # 我的写法，时间复杂度比较大
-        if not nums:
-            return 0
-        dp = [0] * len(nums)
-        if nums[0] == 1:
-            dp[0] = 1
-        for i in range(1, len(nums)):
-            if nums[i] == 1 and nums[i - 1] == 1:
-                dp[i] = dp[i - 1] + 1
-            elif nums[i] == 1 and nums[i - 1] == 0:
-                dp[i] = 1
-        return max(dp)
+        # if not nums:
+        #     return 0
+        # dp = [0] * len(nums)
+        # if nums[0] == 1:
+        #     dp[0] = 1
+        # for i in range(1, len(nums)):
+        #     if nums[i] == 1 and nums[i - 1] == 1:
+        #         dp[i] = dp[i - 1] + 1
+        #     elif nums[i] == 1 and nums[i - 1] == 0:
+        #         dp[i] = 1
+        # return max(dp)
 
 
 print(Solution().findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1]))
