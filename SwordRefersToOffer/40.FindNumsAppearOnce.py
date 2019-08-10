705101b33b349fb435abf00c9318d917da97b776
kemu <2540278344@qq.com>
2019-08-10 23:36:58 +0800

    20190819modify

--- SwordRefersToOffer/40.FindNumsAppearOnce.py
+++ SwordRefersToOffer/40.FindNumsAppearOnce.py
@@ -6,14 +6,26 @@ class Solution:
     # 返回[a,b] 其中ab是出现一次的两个数字
     def FindNumsAppearOnce(self, array):
         # write code here
+        # res = []
+        # s = {}
+        # for i in array:
+        #     if i not in s:
+        #         s[i] = 1
+        #     else:
+        #         s[i] += 1
+        # for key, value in s.items():
+        #     if value == 1:
+        #         res.append(key)
+        # return res
+
         res = []
         s = {}
         for i in array:
-            if i not in s:
-                s[i] = 1
-            else:
-                s[i] += 1
+            s[i] = s.get(i, 0) + 1
         for key, value in s.items():
             if value == 1:
                 res.append(key)
         return res
+
+
+print(Solution().FindNumsAppearOnce([1, 2, 2, 1, 2, 3, 4]))
