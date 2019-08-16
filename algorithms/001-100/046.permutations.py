449d4b15b931f77bc78cfc5d663b1a8d365ed9a7
kemu <2540278344@qq.com>
2019-08-16 22:15:54 +0800

    20190816moodify

--- algorithms/001-100/046.permutations.py
+++ algorithms/001-100/046.permutations.py
@@ -12,35 +12,35 @@ class Solution:
         # return list(itertools.permutations(nums, len(nums)))
 
         # # 递归法
-        res = []
-        if len(nums) <= 1:
-            return [nums]
-        for i in range(len(nums)):
-            num = nums[i]
-            newnums = nums[:i] + nums[i + 1:]
-            for item in self.permute(newnums):
-                res.append([num] + item)
-        return res
+        # res = []
+        # if len(nums) <= 1:
+        #     return [nums]
+        # for i in range(len(nums)):
+        #     num = nums[i]
+        #     newnums = nums[:i] + nums[i + 1:]
+        #     for item in self.permute(newnums):
+        #         res.append([num] + item)
+        # return res
 
         # 回溯法
-        # if not nums:
-        #     return []
-        # res = []
-        # n = len(nums)
-        # visited = [0] * n
+        if not nums:
+            return []
+        res = []
+        n = len(nums)
+        visited = [0] * n
 
-        # def backtrack(i, tmp_list):
-        #     if i == n:
-        #         res.append(tmp_list)
-        #     for j in range(n):
-        #         if visited[j]:
-        #             continue
-        #         visited[j] = 1
-        #         backtrack(i + 1, tmp_list + [nums[j]])
-        #         visited[j] = 0
+        def backtrack(i, tmp_list):
+            if i == n:
+                res.append(tmp_list)
+            for j in range(n):
+                if visited[j]:
+                    continue
+                visited[j] = 1
+                backtrack(i + 1, tmp_list + [nums[j]])
+                visited[j] = 0
 
-        # backtrack(0, [])
-        # return res
+        backtrack(0, [])
+        return res
 
 
 print(Solution().permute([1, 2, 3]))
