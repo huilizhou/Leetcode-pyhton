561f57b086205deafb4ac2294ace72aeb37c5dcf
kemu <2540278344@qq.com>
2019-08-23 22:51:16 +0800

    20190823nodify

--- algorithms/001-100/009.Palindrom number.py
+++ algorithms/001-100/009.Palindrom number.py
@@ -5,7 +5,27 @@ class Solution:
         :type x: int
         :rtype: bool
         """
-        return True if str(x)[::-1] == str(x) else False
+        # return  str(x)[::-1] == str(x)
+
+        # 大神的解法
+        # import math
+        # r = list(map(lambda i: int(10**-i * x % 10),
+        #              range(int(math.log10(x)), -1, -1))) if x > 0 else [0, x]
+        # return r == r[::-1]
+
+        # 不转化成字符串
+        if x == 0:
+            return True
+        elif x < 0 or (x != 0 and x % 10 == 0):
+            return False
+        else:
+            res = 0
+            while x > res:
+                res = 10 * res + x % 10
+                x //= 10
+                if x == res or x == res // 10:
+                    return True
+            return False
 
 
 print(Solution().isPalindrome(-121))
