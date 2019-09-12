94b96a6b369423516980bd4d0609009a0218df14
kemu <2540278344@qq.com>
2019-09-12 19:09:35 +0800

    20190912modifyy

--- algorithms/001-100/064.minimum-path-sum.py
+++ algorithms/001-100/064.minimum-path-sum.py
@@ -6,19 +6,40 @@ class Solution:
         :rtype: int
         """
         # 我的想法，动态规划的解法
-        m = len(grid)  # row
-        n = len(grid[0])  # column
-        for i in range(m):
-            for j in range(n):
-                if i == 0 and j == 0:
-                    grid[i][j] = grid[0][0]
-                elif i == 0 and j > 0:
-                    grid[i][j] += grid[i][j - 1]
-                elif j == 0 and i > 0:
-                    grid[i][j] += grid[i - 1][j]
-                elif i > 0 and j > 0:
-                    grid[i][j] += min(grid[i - 1][j], grid[i][j - 1])
-        return grid[- 1][- 1]
+        # m = len(grid)  # row
+        # n = len(grid[0])  # column
+        # for i in range(m):
+        #     for j in range(n):
+        #         if i == 0 and j == 0:
+        #             grid[i][j] = grid[0][0]
+        #         elif i == 0 and j > 0:
+        #             grid[i][j] += grid[i][j - 1]
+        #         elif j == 0 and i > 0:
+        #             grid[i][j] += grid[i - 1][j]
+        #         elif i > 0 and j > 0:
+        #             grid[i][j] += min(grid[i - 1][j], grid[i][j - 1])
+        # return grid[- 1][- 1]
+
+        # 我的想法
+        '''
+        状态定义dp为mxn矩阵，其中dp[i][j]的值代表直到走到(i,j)的最小路径和。
+
+        转移方程，只能向右或者向下走，即当前单元格(i,j)只能是左方单元格(i-1,j)或上方单元格(i,j-1)走到。
+        因此只需要考虑矩阵左边界和上边界。
+
+        '''
+        m = len(grid)
+        n = len(grid[0])
+        dp = [[0 for _ in range(n)]for _ in range(m)]
+        dp[0][0] = grid[0][0]
+        for i in range(1, m):
+            dp[i][0] = dp[i - 1][0] + grid[i][0]
+        for j in range(1, n):
+            dp[0][j] = dp[0][j - 1] + grid[0][j]
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
+        return dp[-1][-1]
 
 
 print(Solution().minPathSum([
--- algorithms/501-600/518.coin-change-2.py
+++ algorithms/501-600/518.coin-change-2.py
@@ -10,15 +10,40 @@ class Solution(object):
         记录每添加一种面额的零钱，总金额的变化
         例如对于总额5，当只有面额1的零钱时，只有一种可能5*1
         当加了面额为2的零钱时，除了原来那一种可能外，还加上组合了两块的情况。
+
         '''
         dp = [0] * (amount + 1)
         coins.sort()
-
         dp[0] = 1
         for coin in coins:
             for i in range(coin, amount + 1):
-                dp[i] = dp[i] + dp[i - coin]
+                dp[i] += dp[i - coin]
         return dp[amount]
+
+        # 人家的解法
+        '''
+         “完全背包问题”解题有一定的套路和经验。
+         1、状态的定义
+         dp[i][j]：考虑前i个硬币能凑成总金额j的组合数
+         2、状态转移方程
+        dp[i][j] = dp[i - 1][j - 0 * coins[i - 1]] + 
+        dp[i - 1][j - 1 * coins[i - 1]] +
+        dp[i - 1][j - 2 * coins[i - 1]] + 
+        ... + 
+        dp[i - 1][j - k * coins[i - 1]]
+
+        这里 j - k * coins[i - 1] >= 0。
+
+        '''
+        # size = len(coins)
+        # dp = [0 for _ in range(amount + 1)]
+        # dp[0] = 1
+
+        # for i in range(1, size + 1):
+        #     for j in range(amount + 1):
+        #         if j - coins[i - 1] >= 0:
+        #             dp[j] += dp[j - coins[i - 1]]
+        # return dp[amount]
 
 
 print(Solution().change(5,  [1, 2]))
